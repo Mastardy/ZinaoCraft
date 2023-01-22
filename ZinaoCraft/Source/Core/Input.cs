67569da581dce78280037dc2f73c023f0b113baf
André Monteiro <andremgmonteiro01@gmail.com>
2023-01-22 13:38:43 +0000

    Implemented Input as a static class for easier global usage

---
+++ ZinaoCraft/Source/Core/Input.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace ZinaoCraft;
+
+public static class Input
+{
+    public static KeyboardState? keyboardState;
+
+    public static bool GetKeyDown(Keys key)
+    {
+        if (keyboardState == null) return false;
+        return keyboardState.IsKeyPressed(key);
+    }
+
+    public static bool GetKeyUp(Keys key)
+    {
+        if (keyboardState == null) return false;
+        return keyboardState.IsKeyReleased(key);
+    }
+
+    public static bool GetKey(Keys key)
+    {
+        if (keyboardState == null) return false;
+        return keyboardState.IsKeyDown(key);
+    }
+}
